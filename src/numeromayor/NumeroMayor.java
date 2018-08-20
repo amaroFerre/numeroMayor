feb7de75c39fc07b90a1ecd520ac085d5abdf837
Amarico <Amarico@EquipoAcer>
2018-08-20 15:24:38 +0200

    Cambio enunciado del println

--- src/numeromayor/NumeroMayor.java
+++ src/numeromayor/NumeroMayor.java
@@ -44,11 +44,11 @@ public class NumeroMayor {
 
         if ((numero1 > numero2) && (numero1 > numero3)) {
 
-            System.out.println("El número mayor es: " + numero1);
+            System.out.println("El número mayor es el : " + numero1);
 
         } else if ((numero2 > numero1) && (numero2 > numero3)) {
 
-            System.out.println("El núero mayor es: " + numero2);
+            System.out.println("El núero mayor es el : " + numero2);
 
         }else
             
