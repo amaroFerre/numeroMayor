7251d2317ae19d1ab207800038a5ec979a466422
Amarico <Amarico@EquipoAcer>
2018-08-20 15:30:01 +0200

    cambio todos los println

--- src/numeromayor/NumeroMayor.java
+++ src/numeromayor/NumeroMayor.java
@@ -44,15 +44,15 @@ public class NumeroMayor {
 
         if ((numero1 > numero2) && (numero1 > numero3)) {
 
-            System.out.println("El número mayor es el : " + numero1);
+            System.out.println("El número mayor de estos tres números "+ numero1 + " " + numero2 + " " + numero3 + "  es: " + numero1);
 
         } else if ((numero2 > numero1) && (numero2 > numero3)) {
 
-            System.out.println("El número mayor es el : " + numero2);
+            System.out.println("El número mayor de estos tres números "+ numero1 + " " + numero2 + " " + numero3 + "  es: " + numero2);
 
         }else
             
-            System.out.println("El numero " + numero3 + " es mayor");
+            System.out.println("El número mayor de estos tres números "+ numero1 + " " + numero2 + " " + numero3 + "  es: " + numero3 );
     }
     
     
