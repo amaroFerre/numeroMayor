0941f4d02b21f64f537908f68e6e5ae19d0b9a86
Amarico <Amarico@EquipoAcer>
2018-08-20 15:15:30 +0200

    creo el método imprime resultado

--- src/numeromayor/NumeroMayor.java
+++ src/numeromayor/NumeroMayor.java
@@ -8,20 +8,36 @@ package numeromayor;
 // Importo el paquete java.util para utilizar la clase Scanner
 import java.util.*;
 
-
 /**
  *
  * @author Amarico
  */
 public class NumeroMayor {
-    
+
     // Declaro tres variables de tipo entero
-    
     int numero1;
     int numero2;
     int numero3;
-    
-    
+
+    public void ejecutaPrograma() {
+
+        imprimeResultado();
+    }
+    // Método imprimeResultado
+    public void imprimeResultado() {
+
+        if ((numero1 > numero2) && (numero1 > numero3)) {
+
+            System.out.println("El número mayor es: " + numero1);
+
+        } else if ((numero2 > numero1) && (numero2 > numero3)) {
+
+            System.out.println("El núero mayor es: " + numero2);
+
+        }else
+            
+            System.out.println("El numero " + numero3 + " es mayor");
+    }
     
     
     
